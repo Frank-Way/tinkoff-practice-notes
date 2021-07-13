40fc61b6dfd9f71448cbcb338d893ed6f42a44ad
Frank-Way <57298515+Frank-Way@users.noreply.github.com>
2021-07-13 21:01:35 +0300

    hotfix: изменён импорт в UserRepository

--- notes/src/main/java/com/practice/notes/repository/UserRepository.java
+++ notes/src/main/java/com/practice/notes/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.practice.notes.repository;
 
-//import com.practice.notes.model.User;
-import org.apache.tomcat.jni.User;
+import com.practice.notes.model.User;
+//import org.apache.tomcat.jni.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.jta.UserTransactionAdapter;
